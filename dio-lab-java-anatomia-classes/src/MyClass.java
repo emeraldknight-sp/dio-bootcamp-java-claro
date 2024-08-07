07caf11e0f6727d879fea5ecd829e82d66ae2e7e
David Almeida <contato.davidalmeida@outlook.com>
2024-08-07 17:07:31 -0300

    ✅ Update: Anatomy of Classes - Class 03

--- dio-lab-java-anatomia-classes/src/MyClass.java
+++ dio-lab-java-anatomia-classes/src/MyClass.java
@@ -13,6 +13,30 @@ public class MyClass {
     // String &helloWorld = "Hello, World!";
     // String 2helloWorld = "Hello, World!";
 
+    // Definição de variáveis
+    // Tipo + nomeBemDefinido = atribuição (opcional em alguns casos)
+    // String meuNome = "David";
+    // int anoFabricacao = 1999;
+    // boolean verdadeira = true;
+    // anoFabricacao = 2000;
+
+    // Definição de métodos
+    // Tipo de retorno + nomeObjetiboNoInfinitivo + parâmetro(s)
+    // int somar (int numeroUm, int numeroDois)
+    // String formatarCep (long cep)
+
     System.out.println("Hello, World!");
+
+    // Usando métodos e variáveis
+    // String firstName = "David";
+    // String lastName = "Almeida";
+
+    // String fullName = fullName(firstName, lastName);
+
+    // System.out.println(fullName);
   }
+
+  // public static String fullName(String firstName, String lastName) {
+  //   return firstName.concat(" ").concat(lastName);
+  // }
 }
