b850ce3abafe7a8d68616223b20bea4f58516366
David Almeida <contato.davidalmeida@outlook.com>
2024-08-07 17:06:45 -0300

    ✅ Update: Anatomy of Classes - Class 02

--- dio-lab-java-anatomia-classes/src/MyClass.java
+++ dio-lab-java-anatomia-classes/src/MyClass.java
@@ -1,5 +1,18 @@
 public class MyClass {
   public static void main(String[] args) {
+
+    // Declarações válidas
+    // String helloWorld = "Hello, World!";
+    // String hello_World = "Hello, World!";
+    // String helloWorld2 = "Hello, World!";
+    // String $helloWorld = "Hello, World!";
+    // String _helloWorld = "Hello, World!";
+
+    // Declarações inválidas
+    // String hello World = "Hello, World!";
+    // String &helloWorld = "Hello, World!";
+    // String 2helloWorld = "Hello, World!";
+
     System.out.println("Hello, World!");
   }
 }
