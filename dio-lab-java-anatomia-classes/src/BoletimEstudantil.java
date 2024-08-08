60ac99b648a0df9a514965d9fcc29092b6e739e7
David Almeida <contato.davidalmeida@outlook.com>
2024-08-08 20:34:15 -0300

    ✅ Add: Anatomy of Classes - Class 04

---
+++ dio-lab-java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,26 @@
+public class BoletimEstudantil {
+  public static void main(String[] args) {
+
+    // Má identação
+    // int mediaFinal = 6;
+    // if (mediaFinal < 6) {
+    // System.out.println("REPROVADO!");
+    // } else if (mediaFinal == 6) {
+    // System.out.println("PROVA MINERVA");
+    // } else {
+    // System.out.println("APROVADO!");
+    // }
+
+    // Boa identação - Boa prática
+    // int mediaFinal = 6;
+    // if (mediaFinal < 6) {
+    //   System.out.println("REPROVADO!");
+    // } else if (mediaFinal == 6) {
+    //   System.out.println("PROVA MINERVA");
+    // } else {
+    //   System.out.println("APROVADO!");
+    // }
+
+    System.out.println("Boletim Estudantil executado com sucesso.");
+  }
+}
