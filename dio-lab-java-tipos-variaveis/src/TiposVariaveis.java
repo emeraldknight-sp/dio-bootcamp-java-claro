a427dfc7ad0abcbb8ecf6e2fac4b16dc06860d27
David Almeida <contato.davidalmeida@outlook.com>
2024-08-09 16:12:41 -0300

    ✅ Update: Types and variables - Class 04

--- dio-lab-java-tipos-variaveis/src/TiposVariaveis.java
+++ dio-lab-java-tipos-variaveis/src/TiposVariaveis.java
@@ -2,7 +2,7 @@ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         
         int idade = 25;
-        
+
         // Dois mil quinhentos e trinta e três
         double salarioMinimo = 2500.33;
 
@@ -17,5 +17,12 @@ public class TiposVariaveis {
         // Resolve-se com cast quando quero converter uma valor de um tipo
         // com representação maior para representação menor
         // short numeroCurto2 = (short) numeroNormal;
+
+        // Variáveis variam, mudam de valor
+        int numero = 5;
+        numero = 10;
+
+        // Constantes são imutáveis, não mudam o que armazenam
+        final double PI = 3.14;
     }
 }
