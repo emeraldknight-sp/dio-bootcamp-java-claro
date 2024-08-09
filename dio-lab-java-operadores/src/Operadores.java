57c95b42327df0b010bcd035b372f1214b515f31
David Almeida <contato.davidalmeida@outlook.com>
2024-08-09 16:40:54 -0300

    ✅ Update: Operators - Class 02

--- dio-lab-java-operadores/src/Operadores.java
+++ dio-lab-java-operadores/src/Operadores.java
@@ -1,5 +1,41 @@
+import java.util.Date;
+
 public class Operadores {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Usos do operador de atribuição
+        String nome = "David";
+        int idade = 25;
+        double peso = 63;
+        char sexo = 'M';
+        boolean doadorSangue = true;
+        Date dataNascimento = new Date();
+
+        // Usos do operador aritmético
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao =  15 / 3;
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+        
+        concatenacao = 1+1+"1"+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
     }
 }
