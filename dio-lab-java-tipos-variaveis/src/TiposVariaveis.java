6e062b0fa1d2dab37e3f36f617566722771a54d4
David Almeida <contato.davidalmeida@outlook.com>
2024-08-09 16:03:32 -0300

    ✅ Update: Types and variables - Class 03

--- dio-lab-java-tipos-variaveis/src/TiposVariaveis.java
+++ dio-lab-java-tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int idade = 25;
+        
+        // Dois mil quinhentos e trinta e três
+        double salarioMinimo = 2500.33;
+
+        // Dois ponto cinco
+        double metadeDeCinco = 2.500;
+
+        // Exemplo do controle de tipos em Java
+        // short numeroCurto = 1;
+        // int numeroNormal = numeroCurto;
+        // short numeroCurto2 = numeroNormal;
+
+        // Resolve-se com cast quando quero converter uma valor de um tipo
+        // com representação maior para representação menor
+        // short numeroCurto2 = (short) numeroNormal;
     }
 }
