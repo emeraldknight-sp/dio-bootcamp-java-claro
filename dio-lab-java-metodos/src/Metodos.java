5ca21f8099fdb24b174a6938323ebaa1bd3a80d9
David Almeida <contato.davidalmeida@outlook.com>
2024-08-13 15:18:13 -0300

    ✅ Update: Methods - Class 02

---
+++ dio-lab-java-metodos/src/Metodos.java
@@ -0,0 +1,14 @@
+public class Metodos {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // Requisitos para criação de métodos
+        // Ser nomeado em verbo
+        // Escrita em camelCase
+
+        // somar(int n1, int n2) {}
+        // abrirConexao() {}
+        // concluirProcessamento() {}
+        
+    }
+}
