2b8f0e23e6bfd753ebeb6cf3b707e1f579756ab8
David Almeida <contato.davidalmeida@outlook.com>
2024-08-13 16:43:02 -0300

    ✅ Update: Reserved words - Class 02

--- dio-lab-java-palavras-reservadas/src/PalavrasReservadas.java
+++ dio-lab-java-palavras-reservadas/src/PalavrasReservadas.java
@@ -1,5 +1,46 @@
 public class PalavrasReservadas {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        // import
+        // package
+
+        // public
+        // private
+        // protected
+
+        // boolean
+        // byte
+        // char
+        // double
+        // float
+        // int
+        // long
+        // short
+        // void
+
+        // abstract
+        // class
+        // extends
+        // final
+        // implements
+        // interface
+        // native
+        // new
+        // static
+        // strictfp
+        // synchronized
+        // transient
+        // volatile
+    
+        // break
+        // continue
+        // case
+
+        // super
+        // this
+
+        // throws
+        // throw
     }
 }
