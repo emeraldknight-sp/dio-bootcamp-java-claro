e55fbf371c508be40f120ca9baccad8be18e56a2
David Almeida <contato.davidalmeida@outlook.com>
2024-08-14 15:50:04 -0300

    ✅ Update: Java Doc - Class 02

--- dio-lab-java-doc/src/Doc.java
+++ dio-lab-java-doc/src/Doc.java
@@ -1,5 +1,32 @@
-public class App {
+public class Doc {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        // Tags
+
+        // @author
+        // @since
+        // @version
+        // @param
+        // @return
+        // @throws
+
+        // Tipos de comentários
+
+        // One Line - Comentário em uma única linha
+
+        /*
+         * Comentário
+         * de múltiplas linhas
+         */
+
+         /**
+          * Comentário para
+          * aspecto de documentação
+          */
+
+        // Javadoc
+
+        // Ferramenta que gera a documentação sobre o código escrito na linguagem Java.
     }
 }
