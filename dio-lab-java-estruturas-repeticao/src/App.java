2e1f67cdede718d6b4f4471efd4f074b1a4ca3bd
David Almeida <contato.davidalmeida@outlook.com>
2024-08-15 16:31:35 -0300

    ✅ Update: Repetition structures - Class 02

--- dio-lab-java-estruturas-repeticao/src/App.java
+++ dio-lab-java-estruturas-repeticao/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        // Estruturas condicionais
+        // if-else, switch-case
+        
+        // Estruturas de repetição
+        // for, while, do-while
+
+        // Estruturas de exceção
+        // try-catch-finally, throw
     }
 }
