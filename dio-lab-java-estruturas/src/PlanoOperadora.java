6f933d223796cbf581383233b1237f4b89fa1233
David Almeida <contato.davidalmeida@outlook.com>
2024-08-16 16:44:18 -0300

    ✅ Update: Conditionals structures - Class 05

---
+++ dio-lab-java-estruturas/src/PlanoOperadora.java
@@ -0,0 +1,14 @@
+public class PlanoOperadora {
+  public static void main(String[] args) {
+    String plano = "T";
+
+    switch (plano) {
+      case "T":
+        System.out.println("5Gb p/ YouTube");
+      case "M":
+        System.out.println("WhatsApp e Instagram ilimitado");
+      case "B":
+        System.out.println("100 minutos de ligação");
+    }
+  }
+}
