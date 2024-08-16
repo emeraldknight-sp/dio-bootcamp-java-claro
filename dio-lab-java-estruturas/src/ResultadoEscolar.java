4d15dfde177cd4df1eb40c376494df2315b2bbcb
David Almeida <contato.davidalmeida@outlook.com>
2024-08-16 16:16:11 -0300

    ✅ Update: Conditionals structures - Class 04

--- dio-lab-java-estruturas/src/ResultadoEscolar.java
+++ dio-lab-java-estruturas/src/ResultadoEscolar.java
@@ -1,13 +1,21 @@
 public class ResultadoEscolar {
   public static void main(String[] args) {
-    int nota = 6;
+    int nota = 10;
 
-    if (nota >= 7) {
-      System.out.println("Aprovado.");
-    } else if (nota > 5 && nota < 7) {
-      System.out.println("Recuperação");
-    } else {
-      System.out.println("Reprovado.");
-    }
+    // Estrutura de condição encadeada
+
+    // if (nota >= 7) {
+    // System.out.println("Aprovado.");
+    // } else if (nota > 5 && nota < 7) {
+    // System.out.println("Recuperação");
+    // } else {
+    // System.out.println("Reprovado.");
+    // }
+
+    // Estrutura de condição ternária
+
+    String resultado = nota >= 7 ? "Aprovado" : nota > 5 && nota < 7 ? "Recuperação" : "Reprovado";
+
+    System.out.println(resultado);
   }
 }
