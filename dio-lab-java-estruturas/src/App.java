30a5ce816337dd5501665af94b4ba0ef8b033035
David Almeida <contato.davidalmeida@outlook.com>
2024-08-18 08:54:29 -0300

    ✅ Update: Conditionals structures - Class 05

--- dio-lab-java-estruturas/src/App.java
+++ dio-lab-java-estruturas/src/App.java
@@ -1,14 +1,36 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  public static void main(String[] args) throws Exception {
+    System.out.println("Hello, World!");
 
-        // Estruturas condicionais
-        // if-else, switch-case
-        
-        // Estruturas de repetição
-        // for, while, do-while
+    // Estruturas condicionais
+    // if-else, switch-case
 
-        // Estruturas de exceção
-        // try-catch-finally, throw
+    boolean variavel = true;
+
+    if (variavel) {
+      System.out.println("Verdadeiro.");
+    } else {
+      System.out.println("Falso.");
     }
+
+    final int verificador = 18;
+    String idade = 17 >= verificador ? "Maior de idade" : "Menor de idade";
+
+    switch (idade) {
+      case "Maior de idade":
+        System.out.println("Tem mais ou menos 18 anos");
+        break;
+      case "Menor de idade":
+        System.out.println("Tem menos de 18 anos");
+        break;
+      // default:
+      // break;
+    }
+
+    // Estruturas de repetição
+    // for, while, do-while
+
+    // Estruturas de exceção
+    // try-catch-finally, throw
+  }
 }
