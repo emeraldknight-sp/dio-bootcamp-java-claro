9564cf83066711c13834df928674be8ad42b87e0
David Almeida <contato.davidalmeida@outlook.com>
2024-08-20 16:23:49 -0300

    ✅ Update: Project - Class 02

--- dio-lab-project-controle-candidatos/src/edu/candidatuas/ProcessoSeletivo.java
+++ dio-lab-project-controle-candidatos/src/edu/candidatuas/ProcessoSeletivo.java
@@ -1,11 +1,37 @@
 package edu.candidatuas;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
   public static void main(String[] args) {
     System.out.println("Processo Seletivo.");
-    analisarCandidato(1900.0);
-    analisarCandidato(2200.0);
-    analisarCandidato(2000.0);
+    // analisarCandidato(1900.0);
+    // analisarCandidato(2200.0);
+    // analisarCandidato(2000.0);
+  }
+
+  static void selecaoCandidatos() {
+    String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "MARCELA", "JULIANO", "RAFAEL", "SERGIO", "PAULA", "RESENDE" };
+    int candidatosSelecionados = 0;
+    int candidatoAtual = 0;
+    double salarioBase = 2000.0;
+
+    while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+      String candidato = candidatos[candidatoAtual];
+      double salarioPretendido = valorPretendido();
+
+      System.out.println("O candidato " + candidato + " solicitou este valor de salário " + salarioPretendido);
+
+      if (salarioBase >= salarioPretendido) {
+        System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+        candidatosSelecionados++;
+      }
+      candidatoAtual++;
+    }
+  }
+
+  static double valorPretendido() {
+    return ThreadLocalRandom.current().nextDouble(1800, 2200);
   }
 
   static void analisarCandidato(double salarioPretendido) {
