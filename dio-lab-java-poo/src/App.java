87169cd573a93fa20b77b900014e0bf499e64381
David Almeida <contato.davidalmeida@outlook.com>
2024-08-31 17:01:26 -0300

    ✅ Update: POO - Class 06

--- dio-lab-java-poo/src/App.java
+++ dio-lab-java-poo/src/App.java
@@ -3,14 +3,15 @@ public class App {
         System.out.println("Hello, World!");
     }
 
-    // Visibilidade de recurso
+    // VISIBILIDADE DE RECURSO
 
     // public: O membro (classe, método ou variável) é acessível
     // de qualquer outra classe em qualquer pacote.
     // Isso proporciona o maior nível de acesso.
 
     // protected: O membro é acessível apenas dentro da mesma classe,
-    // classes no mesmo pacote e subclasses (mesmo que estejam em pacotes diferentes).
+    // classes no mesmo pacote e subclasses (mesmo que estejam em pacotes
+    // diferentes).
     // É menos permissivo que public, mas mais acessível que default.
 
     // default (ou package-private): Se nenhum modificador de acesso é especificado,
@@ -20,4 +21,25 @@ public class App {
     // private: O membro é acessível apenas dentro da própria classe.
     // É o nível mais restritivo de acesso, garantindo que outras classes
     // não possam acessar diretamente o membro.
+
+    // CONSTRUTORES
+
+    // Um construtor é definido dentro da classe
+    // com o mesmo nome da classe e sem tipo de retorno.
+
+    int valor;
+
+    // Construtor
+    public App(int valor) {
+        this.valor = valor;
+    }
+
+    // Construtor Padrão: Se você não definir nenhum construtor, o Java fornece um
+    // construtor padrão sem parâmetros que simplesmente cria o objeto sem
+    // inicializar variáveis.
+    
+    // Sem Construtor Definido: Se você não definir um construtor, o Java cria um
+    // construtor padrão automaticamente. No entanto, se você definir um construtor
+    // personalizado, o construtor padrão não estará mais disponível, e você terá
+    // que definir um construtor sem parâmetros se precisar de um.
 }
