01a43b8aca5f697970a1176c30babe1a92e05969
David Almeida <contato.davidalmeida@outlook.com>
2024-09-13 16:57:04 -0300

    ✅ Update: Challenge from Class 04

--- dio-lab-java-collections/src/edu/java/collections/list/OperacoesBasicas/CarrinhoDeCompras.java
+++ dio-lab-java-collections/src/edu/java/collections/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -10,8 +10,8 @@ public class CarrinhoDeCompras {
     this.carrinho = new ArrayList<>();
   }
 
-  public void adicionarItem(String nome) {
-    carrinho.add(new Item(nome));
+  public void adicionarItem(String nome, double preco, int quantidade) {
+    carrinho.add(new Item(nome, preco, quantidade));
   }
 
   public void removerItem(String nome) {
@@ -25,29 +25,36 @@ public class CarrinhoDeCompras {
     carrinho.removeAll(itensParaRemover);
   }
 
-  public int obterNumeroItens() {
-    return carrinho.size();
+  public double calcularValorTotal() {
+    double valorTotal = 0;
+
+    if (!carrinho.isEmpty()) {
+      for (Item i : carrinho) {
+        valorTotal += i.getPreco() * i.getQuantidade();
+      }
+    }
+    return valorTotal;
   }
 
-  public void obterNomeItens() {
+  public void exibirItens() {
     System.out.println(carrinho);
   }
 
   public static void main(String[] args) {
     CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
 
-    carrinho.adicionarItem("Vassoura");
-    carrinho.adicionarItem("Água sanitária");
-    carrinho.adicionarItem("Desinfetante");
-    carrinho.adicionarItem("Esponja");
-    carrinho.adicionarItem("Odorizador");
+    carrinho.adicionarItem("Água de côco", 5.73, 4);
+    carrinho.adicionarItem("Energético", 6.50, 2);
+    carrinho.adicionarItem("Kit Coquetelaria 2.0", 49.90, 1);
+    carrinho.adicionarItem("Whisky Black Label", 129.9, 2);
+    carrinho.adicionarItem("Gelo", 3.75, 5);
 
-    System.out.println("Quantidade de itens no carrinho: " + carrinho.obterNumeroItens());
-    carrinho.obterNomeItens();
+    System.out.println("Valor total: R$" + carrinho.calcularValorTotal());
+    carrinho.exibirItens();
 
-    carrinho.removerItem("Odorizador");
-    
-    System.out.println("Quantidade de itens no carrinho: " + carrinho.obterNumeroItens());
-    carrinho.obterNomeItens();
+    carrinho.removerItem("Gelo");
+
+    System.out.println("Valor total: R$" + carrinho.calcularValorTotal());
+    carrinho.exibirItens();
   }
 }
