91a788c422664b16c1cb044c527f931610c59ebc
David Almeida <contato.davidalmeida@outlook.com>
2024-09-17 16:47:30 -0300

    ✅ Update: Collections (Set) - Class 07

--- dio-lab-java-collections/src/edu/java/collections/set/Ordenacao/CadastroProduto.java
+++ dio-lab-java-collections/src/edu/java/collections/set/Ordenacao/CadastroProduto.java
@@ -22,7 +22,25 @@ public class CadastroProduto {
 
   public Set<Produto> exibirProdutosPorPreco() {
     Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
-    inventario.addAll(inventario);
+    produtosPorPreco.addAll(inventario);
     return produtosPorPreco;
+  }
+
+  public static void main(String[] args) {
+    CadastroProduto inventario = new CadastroProduto();
+    inventario.adicionarProduto(1, "Agua Mineral s/ Gás", 1.50, 10);
+    inventario.adicionarProduto(2, "Agua Mineral c/ Gás", 2.50, 5);
+    inventario.adicionarProduto(3, "Refrigerante Coca Cola", 2.75, 7);
+    inventario.adicionarProduto(4, "Refrigerante Fanta Laranja", 3.10, 15);
+    inventario.adicionarProduto(5, "Refrigerante Fanta Uva", 1.00, 20);
+    inventario.adicionarProduto(6, "Refrigerante Sprite", 6.25, 13);
+    inventario.adicionarProduto(2, "Biscoito", 2.75, 22);
+    inventario.adicionarProduto(4, "Bolacha", 1.59, 17);
+
+    System.out.println(inventario.inventario);
+
+    System.out.println(inventario.exibirProdutosPorNome());
+
+    System.out.println(inventario.exibirProdutosPorPreco());
   }
 }
