e06c27f8395099419f29cf832c923cd3ec8fd00b
David Almeida <contato.davidalmeida@outlook.com>
2024-09-18 14:39:25 -0300

    ✅ Add: Collections (Map) - Class 02

---
+++ dio-lab-java-collections/src/edu/java/collections/map/OperacoesBasicas/AgendaContatos.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+  private Map<String, Integer> agenda;
+
+  public AgendaContatos() {
+    this.agenda = new HashMap<>();
+  }
+
+  public void adicionarContato(String nome, Integer numero) {
+    agenda.put(nome, numero);
+  }
+
+  public void removerContato(String nome) {
+    if (!agenda.isEmpty()) {
+      agenda.remove(nome);
+    }
+  }
+
+  public void exibirContatos() {
+    System.out.println(agenda);
+  }
+
+  public Integer pesquisarPorNome(String nome) {
+    Integer numeroPorNome = null;
+    if (!agenda.isEmpty()) {
+      numeroPorNome = agenda.get(nome);
+    }
+    return numeroPorNome;
+  }
+
+}
