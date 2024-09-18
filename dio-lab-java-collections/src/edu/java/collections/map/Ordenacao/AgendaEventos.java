9f8c025ad4f257f35fb0c0f4a56825560d6435bf
David Almeida <contato.davidalmeida@outlook.com>
2024-09-18 16:29:11 -0300

    ✅ Update: Collections (Map) - Class 07.1

--- dio-lab-java-collections/src/edu/java/collections/map/Ordenacao/AgendaEventos.java
+++ dio-lab-java-collections/src/edu/java/collections/map/Ordenacao/AgendaEventos.java
@@ -19,7 +19,26 @@ public class AgendaEventos {
     System.out.println(eventosOrdenados);
   }
 
+  public void obterProximoEvento() {
+    LocalDate dataAtual = LocalDate.now();
+    LocalDate proximaData = null;
+    Evento proximaEvento = null;
+    Map<LocalDate, Evento> eventosOrdenados = new TreeMap<>(eventos);
+    for (Map.Entry<LocalDate, Evento> entry : eventos.entrySet()) {
+      if (entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)) {
+        proximaData = entry.getKey();
+        proximaEvento = entry.getValue();
+        System.out.println("O próximo evento: " + proximaEvento + " acontecerá na data " + proximaData);
+        break;
+      }
+    }
+  }
+
   public static void main(String[] args) {
     AgendaEventos agendaEventos = new AgendaEventos();
+
+    agendaEventos.adicionarEvento(null, null, null);
+
+    agendaEventos.exibirAgenda();
   }
 }
